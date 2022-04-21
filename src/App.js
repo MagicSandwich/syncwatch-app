4239cb3f35ef488542184d146135cd3a48059214
MagicSandwich <pootismen8@gmail.com>
2022-04-21 20:09:00 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import './css/App.css';
 import YTPlayer from "./components/yt-plyr/index.jsx"
 import Chat from "./components/chat/index.jsx"
 import io from 'socket.io-client';
+import { useState } from 'react';
 const { uniqueNamesGenerator, colors, animals } = require('unique-names-generator');
 const socket = io.connect("http://localhost:3001")
 const randomName = uniqueNamesGenerator({ dictionaries: [colors, animals] }); // big_red_donkey
 
 
-function App() {
+function App() {  
   const room = "tba"
+  const [check, setCheck] = useState(false);
+
+
   return (
     <div className="App">
+      {check?(<div className="App">
       <YTPlayer embedId="0i1kgsmVE6Y"></YTPlayer>
       <Chat socket={socket} room={room} username={randomName} ></Chat>
+        
+        </div>):(<div className="Start">
+          <h1> SYNCWATCH</h1>
+          <button onClick={(e) => setCheck({check: !check.value})}>BEGIN</button>
+          </div>)}
     </div>
     
   );
