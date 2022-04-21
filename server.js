ec47ef775aeea74a70057a9bdbd64e4d0727abad
MagicSandwich <pootismen8@gmail.com>
2022-04-21 23:11:22 +0600

    Update 2

--- server.js
+++ server.js
@@ -3,27 +3,30 @@ const app = express()
 const http = require('http')
 const cors = require('cors')
 const {Server} = require('socket.io')
+const helmet = require("helmet");
+const PORT = process.env.PORT || 3001;
 
 app.use(cors({ origin: "*" }));
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
+app.use(express.static(path.join(__dirname, "./build")));
 
 const server = http.createServer(app)
-const io = new Server(server, {
-    cors:{
-        //origin: "http://localhost:3000",
-        origin: "*",
-        methods:["GET","POST"],
-    },
-})
-
-server.listen(3001, ()=>{
-    console.log("SERVER GOOD")
+server.listen(PORT, ()=>{
+    console.log(`Server listening on ${PORT}`)
 });
+
+const io = new Server(server, { cors: { origin: "*" } });
+
 
 io.on('connection',socket =>{
     console.log(socket.id)
     socket.join("tba")
     socket.on('chat-msg',data =>{
-        socket.to(data.room).emit("receive-msg",data)
+        socket.broadcast.to(data.room).emit("receive-msg",data)
         console.log(socket.id,data.msg,data.user,data.room)
     })
 
--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Chat from "./components/chat/index.jsx"
 import io from 'socket.io-client';
 import { useState } from 'react';
 const { uniqueNamesGenerator, colors, animals } = require('unique-names-generator');
-const socket = io.connect("http://localhost:3001")
+//const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://syncwatch-app.herokuapp.com/")
+
 const randomName = uniqueNamesGenerator({ dictionaries: [colors, animals] }); // big_red_donkey
 
 
@@ -16,6 +18,7 @@ function App() {
   return (
     <div className="App">
       {check?(<div className="App">
+      <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
       <YTPlayer embedId="0i1kgsmVE6Y"></YTPlayer>
       <Chat socket={socket} room={room} username={randomName} ></Chat>
         
